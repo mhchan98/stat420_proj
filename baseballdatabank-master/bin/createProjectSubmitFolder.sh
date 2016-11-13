5463d857dd60a11159c9bf66169ace499685d637
Michael Chan <Michael Chan>
2016-11-13 17:07:10 -0500

    Commit from Aaron Rogers and add readme files to target directory

--- baseballdatabank-master/bin/createProjectSubmitFolder.sh
+++ baseballdatabank-master/bin/createProjectSubmitFolder.sh
@@ -1,7 +1,7 @@
 PROJ_DIR=.
 PROJ_OUT_DIR=$PROJ_DIR/target/stat420_proj_final
 PROJ_SRC_DIR=$PROJ_DIR/baseballdatabank-master/core
-PROJ_DATA_FILES="Teams.csv Salaries.csv"
+PROJ_DATA_FILES="Teams.csv Salaries.csv readme2014.txt"
 mkdir -p $PROJ_OUT_DIR
 cp -p $PROJ_SRC_DIR/stat420_proj.* $PROJ_OUT_DIR
 for file in $PROJ_DATA_FILES; do 
