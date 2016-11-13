6c72a074455c955badea41ec7585dc380fe9c279
Michael Chan <Michael Chan>
2016-11-13 15:26:47 -0500

    Fix grammer in the proposal and add script to package required files for submissionwq

---
+++ baseballdatabank-master/bin/createProjectSubmitFolder.sh
@@ -0,0 +1,9 @@
+PROJ_DIR=.
+PROJ_OUT_DIR=$PROJ_DIR/target/stat420_proj_final
+PROJ_SRC_DIR=$PROJ_DIR/baseballdatabank-master/core
+PROJ_DATA_FILES="Teams.csv Salaries.csv"
+mkdir -p $PROJ_OUT_DIR
+cp -p $PROJ_SRC_DIR/stat420_proj.* $PROJ_OUT_DIR
+for file in $PROJ_DATA_FILES; do 
+	cp -p $PROJ_SRC_DIR/$file $PROJ_OUT_DIR
+done
